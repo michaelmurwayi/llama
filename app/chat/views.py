5c989a3d3d3808d26b16c95e6e73fee12b47de61
Michael Murwayi <mikemurwayi7@gmail.com>
2023-06-22 15:38:37 -0400

    loading template to backend view

--- app/chat/views.py
+++ app/chat/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 
 # Create your views here.
+class ChatView(TemplateView):
+    template_name = "chat.html"
+
+    
